0384d383e890c69364883c4e857bdbb1c8f396e1
elzer-pixel <elzer.mokoena@gmail.com>
2025-05-21 14:26:38 +0200

    commit

---
+++ chapter07/rollercoaster.py
@@ -0,0 +1,7 @@
+height = input("How tall are you, in meters? ")
+height = int(height)
+
+if height >= 144:
+    print("\nYou are tall enough to ride!")
+else: 
+    print("\nYou'll be able to ride when your are a little older.")
